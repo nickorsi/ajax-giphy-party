baaaf89fa1f377fbccba519ba7ec83ca42e8b882
Charles Chitty <charlescchitty@gmail.com>
2023-11-14 16:53:07 -0800

    Sent Input data to GIPHY

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("Let's get this party started!");
+
+// $("form").on("submit", requestGIPHY);
+$(".submit").on("click", function (evt){
+  requestGiphy();
+  evt.preventDefault();
+});
+
+async function requestGiphy(){
+  const input = $("#search").val()
+  console.log(input);
+
+  const params = new URLSearchParams(input);
+  console.log(params)
+  let response = await fetch(
+    `http://api.giphy.com/v1/gifs/search?q=${params}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`,
+    { method: "GET" });
+  let text = await response.text();
+
+  console.log(text)
+
+  //use fetch to make a request to GIPHY for info based on term
+  //receive response, console.log response data
+  // evt.preventDefault();
+
+}
